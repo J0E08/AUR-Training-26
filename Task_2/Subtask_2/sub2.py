4131c3f48fb6325f6d81ea5ab8d7d173d8b03e71
J0E08 <youssef.112008.mohamed@gmail.com>
2026-08-28 09:06:01 +0300

    Delete Task_2 directory

--- Task_2/Subtask_2/sub2.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image # type: ignore
-def main():
- image = Image.open("img.png")
- bw_image = image.convert("L")
- bw_image.save("bw.png")
- print("bw.pg is saved in Subtask_2")
- bw_image.show()
- 
-main()
